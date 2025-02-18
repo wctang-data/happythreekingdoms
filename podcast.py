9531a72ce250341ea81fc39411f4570db41304f0
wctang <wctang@gmail.com>
2025-02-19 04:38:33 +0800

    update.

--- podcast.py
+++ podcast.py
@@ -3,25 +3,28 @@
 import os
 
 def main():
+    base = "https://wctang-data.github.io/happythreekingdoms"
     with open("feed.xml", "w", encoding="utf-8") as out:
-        print('''<!DOCTYPE html>
+        print(f'''<!DOCTYPE html>
 <html>
 <head>
 <title>歡樂三國志</title>
 </head>
 <body>
 <h1>歡樂三國志</h1>
-<img src="logo.jpg" />
+<img src="{base}/logo.jpg" />
 <p>歡樂三國志</p>
-<a href="feed.xml">feed</a>
+<a href="{base}/feed.xml">feed</a>
 <ul>''', file=out)
 
         for dirpath, _, filenames in os.walk("."):
             if dirpath == ".":
                 continue
+            elif dirpath.startswith(".\\.git"):
+                continue
             dirpath = dirpath[2:]
             for filename in filenames:
-                print(f'<li><a href="{dirpath}/{filename}">{dirpath} {filename}</a></li>', file=out)
+                print(f'<li><a href="{base}/{dirpath}/{filename}">{dirpath} {filename}</a></li>', file=out)
 
         print('''</ul>
 </body>
